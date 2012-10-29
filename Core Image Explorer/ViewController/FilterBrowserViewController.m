89cbe5750a1f3b3c1aa2f6e66308efe528dd8a13
Joshua Sullivan <jsulliva@nerdery.com>
2012-10-29 09:33:40 -0500

    Added CICrop to the exclusion list for now.

--- Core Image Explorer/ViewController/FilterBrowserViewController.m
+++ Core Image Explorer/ViewController/FilterBrowserViewController.m
@@ -41,7 +41,7 @@ typedef enum {
 {
     [super viewDidLoad];
 	
-    self.exclusionList = @[@"CIColorCube", @"CIMaskToAlpha"];
+    self.exclusionList = @[@"CIColorCube", @"CIMaskToAlpha", @"CICrop"];
     
     self.tableMode = self.modeControl.selectedSegmentIndex;
     
